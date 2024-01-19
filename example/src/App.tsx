6d73f5e2beb9f47d3de6c69686c0e8c5bcb57fee
Stefan  Stevanović <stefan.stevanovic@gerniks.com>
2024-01-19 16:14:49 +0100

    add initial setup methods for MappIntelligence

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,38 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-mappinteligence-plugin';
+import { StyleSheet, View, Text, Button, type GestureResponderEvent } from 'react-native';
+import * as MappIntelligence from 'react-native-mappinteligence-plugin';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    MappIntelligence. multiply(3, 7).then(setResult);
   }, []);
+
+  function onPressLearnMore(): void {
+    MappIntelligence.setAnonymousTracking(false);
+    MappIntelligence.initWithConfiguration([794940687426749], "http://tracker-int-01.webtrekk.net").then(setResult);
+    MappIntelligence.setLogLevel(1);
+    MappIntelligence.setBatchSupportEnabled(false);
+    MappIntelligence.setBatchSupportSize(150);
+    MappIntelligence.setRequestInterval(1);
+    MappIntelligence.setRequestPerQueue(300);
+    MappIntelligence.setShouldMigrate(true);
+    MappIntelligence.setSendAppVersionInEveryRequest(true);
+    MappIntelligence.setEnableBackgroundSendout(true);
+    MappIntelligence.setEnableUserMatching(false);
+  }
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <Button
+        onPress={onPressLearnMore}
+        title="Init MappIntelligence"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
--- ios/MappinteligencePlugin.mm
+++ ios/MappinteligencePlugin.mm
@@ -1,4 +1,7 @@
 #import "MappinteligencePlugin.h"
+#import "MappIntelligence.h"
+#import "MappIntelligenceLogger.h"
+#import <Foundation/Foundation.h>
 
 @implementation MappinteligencePlugin
 RCT_EXPORT_MODULE()
@@ -15,5 +18,118 @@ RCT_EXPORT_METHOD(multiply:(double)a
     resolve(result);
 }
 
+RCT_EXPORT_METHOD(initWithConfiguration:(NSArray*)trackIDs
+                                        domain:(NSString*)domain
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] initWithConfiguration:trackIDs onTrackdomain:domain];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setLogLevel:(NSInteger)level
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setLogLevel:(logLevel)level];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setRequestInterval:(NSInteger)interval
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setRequestInterval:60];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setBatchSupportEnabled:(BOOL)enabled
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setBatchSupportEnabled:enabled];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setEnableBackgroundSendout:(BOOL)enabled
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setEnableBackgroundSendout:true];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setBatchSupportSize:(NSInteger)size
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setBatchSupportSize:size];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setRequestPerQueue:(NSInteger)numberOfRequsts
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setRequestPerQueue:numberOfRequsts];
+    });
+    resolve(@1);
+}
+
+
+RCT_EXPORT_METHOD(setShouldMigrate:(BOOL)migrate
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setShouldMigrate:migrate];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setAnonymousTracking:(BOOL)anonymous
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setAnonymousTracking:anonymous];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setSendAppVersionInEveryRequest:(BOOL)flag
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setSendAppVersionInEveryRequest:flag];
+    });
+    resolve(@1);
+}
+
+RCT_EXPORT_METHOD(setEnableUserMatching:(BOOL)enabled
+                                        resolve:(RCTPromiseResolveBlock)resolve
+                                        reject:(RCTPromiseRejectBlock)reject)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[MappIntelligence shared] setEnableUserMatching:enabled];
+    });
+    resolve(@1);
+}
+
+//[[MappIntelligence shared] setEnableUserMatching:true];
 
 @end
