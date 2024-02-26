abfe2009a23b26a5f43388b295015efbc5d83b79
Semsudin Tafilovic <sekitafilovic@gmail.com>
2024-02-26 18:24:53 +0100

    Updated dependencies for android

--- android/src/main/java/com/mappinteligenceplugin/MappinteligencePluginPackage.kt
+++ android/src/main/java/com/mappinteligenceplugin/MappinteligencePluginPackage.kt
@@ -1,6 +1,7 @@
 package com.mappinteligenceplugin
 
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptModule
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
@@ -9,6 +10,10 @@ import com.facebook.react.uimanager.ViewManager
 class MappinteligencePluginPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
     return listOf(MappinteligencePluginModule(reactContext))
+  }
+
+  override fun createJSModules(): MutableList<Class<out JavaScriptModule>> {
+    return mutableListOf()
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
