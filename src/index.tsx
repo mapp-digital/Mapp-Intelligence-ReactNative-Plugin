408811da594db18502e3c0b129ddf6461a5b069f
Stefan Stevanovic <stefan.stevanovic@192.168.1.13>
2024-03-21 19:10:39 +0100

    change value for convertor of event parameters

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ import type {
 import {
   convertCampaignParameters,
   convertEcommerceParameters,
+  convertEventParameters,
   convertMediaEvent,
   convertPageParameters,
   convertSessionParamters,
