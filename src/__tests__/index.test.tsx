08faecfe2312e813d32fe0615d962a3dfa296056
Semsudin Tafilovic <sekitafilovic@gmail.com>
2024-04-18 21:03:58 +0200

    Added unit tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,34 +1,35 @@
+import { ExceptionType, type MediaEvent } from '../DataTypes';
 import {
   MappIntelligencePlugin,
   build,
   initWithConfiguration,
-  // setLogLevel,
-  // setExceptionLogLevel,
-  // setRequestInterval,
-  // setBatchSupportEnabled,
-  // setEnableBackgroundSendout,
-  // setBatchSupportSize,
-  // setRequestPerQueue,
-  // setShouldMigrate,
-  // setAnonymousTracking,
-  // setSendAppVersionInEveryRequest,
+  setLogLevel,
+  setExceptionLogLevel,
+  setRequestInterval,
+  setBatchSupportEnabled,
+  setEnableBackgroundSendout,
+  setBatchSupportSize,
+  setRequestPerQueue,
+  setShouldMigrate,
+  setAnonymousTracking,
+  setSendAppVersionInEveryRequest,
   // setEnableUserMatching,
-  // trackPage,
-  // trackCustomPage,
-  // trackPageWithCustomData,
-  // trackAction,
-  // trackUrl,
-  // trackMedia,
-  // trackException,
-  // trackExceptionWithName,
-  // setEverId,
-  // getEverId,
-  // isInitialized,
-  // setTemporarySessionId,
-  // optOut,
-  // optIn,
-  // reset,
-  // sendRequestsAndClean,
+  trackPage,
+  trackCustomPage,
+  trackPageWithCustomData,
+  trackAction,
+  trackUrl,
+  trackMedia,
+  trackException,
+  trackExceptionWithName,
+  setEverId,
+  getEverId,
+  isInitialized,
+  setTemporarySessionId,
+  optOut,
+  optIn,
+  reset,
+  sendRequestsAndClean,
 } from '../index';
 
 // Mock the native module
@@ -39,7 +40,10 @@ jest.mock('react-native', () => ({
   NativeModules: {
     MappinteligencePlugin: {
       build: jest.fn(),
-      initWithConfiguration: jest.fn(),
+      initWithConfiguration: jest.fn((ids, domain) => {
+        console.log(ids, domain);
+        return true;
+      }),
       setLogLevel: jest.fn(),
       setExceptionLogLevel: jest.fn(),
       setRequestInterval: jest.fn(),
@@ -75,21 +79,225 @@ describe('MappIntelligencePlugin', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
+  afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+  });
 
-  test('build', async () => {
+  it('build method is called', async () => {
     await build();
     expect(MappIntelligencePlugin.build).toHaveBeenCalled();
   });
 
-  test('initWithConfiguration', async () => {
+  it('init with configuration to return success result and to native method is called', async () => {
     const trackIDs = [123, 456];
     const domain = 'https://example.com';
-    await initWithConfiguration(trackIDs, domain);
+    const result = await initWithConfiguration(trackIDs, domain);
+    expect(result).toBe(true);
     expect(MappIntelligencePlugin.initWithConfiguration).toHaveBeenCalledWith(
       trackIDs,
       domain
     );
   });
 
-  // Add tests for other methods similarly
+  it('set log level for console output', async () => {
+    await setLogLevel(1);
+    expect(MappIntelligencePlugin.setLogLevel).toBeCalledWith(1);
+  });
+
+  it('set exception log level', async () => {
+    await setExceptionLogLevel(ExceptionType.all);
+    expect(MappIntelligencePlugin.setExceptionLogLevel).toBeCalledWith(4); // 4 is numeric value for ExceptionType.all
+  });
+
+  it('set request interval', async () => {
+    await setRequestInterval(15);
+    expect(MappIntelligencePlugin.setRequestInterval).toBeCalledWith(15);
+  });
+
+  it('set batch support', async () => {
+    await setBatchSupportEnabled(true);
+    expect(MappIntelligencePlugin.setBatchSupportEnabled).toBeCalledWith(true);
+  });
+
+  it('set enable background sendout', async () => {
+    await setEnableBackgroundSendout(true);
+    expect(MappIntelligencePlugin.setEnableBackgroundSendout).toBeCalledWith(
+      true
+    );
+  });
+
+  it('set batch size', async () => {
+    await setBatchSupportSize(100);
+    expect(MappIntelligencePlugin.setBatchSupportSize).toBeCalledWith(100);
+  });
+
+  it('set request per queue', async () => {
+    await setRequestPerQueue(10);
+    expect(MappIntelligencePlugin.setRequestPerQueue).toBeCalledWith(10);
+  });
+
+  it('set should migrate', async () => {
+    await setShouldMigrate(false);
+    expect(MappIntelligencePlugin.setShouldMigrate).toBeCalledWith(false);
+  });
+
+  it('set request interval', async () => {
+    await setRequestInterval(15);
+    expect(MappIntelligencePlugin.setRequestInterval).toBeCalledWith(15);
+  });
+
+  it('set anonymous tracking', async () => {
+    await setAnonymousTracking(true);
+    expect(MappIntelligencePlugin.setAnonymousTracking).toBeCalledWith(true);
+  });
+
+  it('set send app version in every request', async () => {
+    await setSendAppVersionInEveryRequest(true);
+    expect(
+      MappIntelligencePlugin.setSendAppVersionInEveryRequest
+    ).toBeCalledWith(true);
+  });
+
+  it('track page', async () => {
+    await trackPage('Test page');
+    expect(MappIntelligencePlugin.trackPage).toBeCalledWith('Test page');
+  });
+
+  it('track custom page', async () => {
+    await trackCustomPage('Test page');
+    expect(MappIntelligencePlugin.trackCustomPage).toBeCalledWith(
+      'Test page',
+      null,
+      null,
+      null,
+      null,
+      null
+    );
+  });
+
+  it('track page with a custom data', async () => {
+    const pageParams: Map<string, string> = new Map([['1', 'PageParam1']]);
+    await trackPageWithCustomData('Test page', pageParams);
+    expect(MappIntelligencePlugin.trackPageWithCustomData).toBeCalledWith(
+      { '1': 'PageParam1' },
+      'Test page'
+    );
+  });
+
+  it('track action', async () => {
+    await trackAction('Test Action');
+    expect(MappIntelligencePlugin.trackAction).toBeCalledWith(
+      'Test Action',
+      null,
+      null,
+      null,
+      null,
+      null
+    );
+  });
+
+  it('track url', async () => {
+    await trackUrl('www.testurl.com', 'abc');
+    expect(MappIntelligencePlugin.trackUrl).toBeCalledWith(
+      'www.testurl.com',
+      'abc'
+    );
+  });
+
+  it('track media', async () => {
+    const mediaEvent: MediaEvent = {
+      pageName: 'Test page',
+      parameters: null,
+    };
+    await trackMedia(mediaEvent);
+    expect(MappIntelligencePlugin.trackMedia).toBeCalledWith({
+      pageName: 'Test page',
+      parameters: null,
+      customParameters: null,
+      eCommerceParameters: null,
+      eventParameters: null,
+      sessionParameters: null,
+    });
+  });
+
+  it('track exception', async () => {
+    const e: Error = {
+      name: 'CustomException',
+      message: 'Testing Exception',
+    };
+
+    await trackException(e, '');
+    expect(MappIntelligencePlugin.trackException).toBeCalledWith(
+      'CustomException',
+      'Testing Exception',
+      ''
+    );
+  });
+
+  it('track exception with name', async () => {
+    await trackExceptionWithName(
+      'TestException',
+      'Handled test exception',
+      'Exception in a unit testing'
+    );
+
+    expect(MappIntelligencePlugin.trackException).toBeCalledWith(
+      'TestException',
+      'Handled test exception',
+      'Exception in a unit testing'
+    );
+  });
+
+  it('set EverID', async () => {
+    await setEverId('123456789');
+    expect(MappIntelligencePlugin.setEverId).toBeCalledWith('123456789');
+  });
+
+  it('get EverID', async () => {
+    const getEverIdSpy = jest.fn(getEverId);
+    getEverIdSpy.mockResolvedValue('123456');
+    const result = await getEverIdSpy();
+    expect(result).toBe('123456');
+  });
+
+  it('Check if plugin initialized', async () => {
+    const isInitializedSpy = jest.fn(isInitialized);
+    isInitializedSpy.mockResolvedValue(true);
+    const result = await isInitializedSpy();
+    expect(result).toBe(true);
+  });
+
+  it('Set temporarySessionId', async () => {
+    const spy = jest.fn(setTemporarySessionId);
+    spy.mockResolvedValue(1);
+
+    const result = await spy('1234');
+    expect(result).toBe(1);
+    expect(spy).toBeCalledWith('1234');
+  });
+
+  it('Opt out', async () => {
+    const spy = jest.fn(optOut).mockResolvedValue(1);
+    const result = await spy(true);
+    expect(result).toBe(1);
+    expect(spy).toBeCalledWith(true);
+  });
+
+  it('Opt in', async () => {
+    const spy = jest.fn(optIn).mockResolvedValue(1);
+    const result = await spy(true);
+    expect(result).toBe(1);
+    expect(spy).toBeCalledWith(true);
+  });
+
+  it('Reset', async () => {
+    await reset();
+    expect(MappIntelligencePlugin.reset).toBeCalled();
+  });
+
+  it('Send requests and clean', async () => {
+    await sendRequestsAndClean();
+    expect(MappIntelligencePlugin.sendRequestsAndClean).toBeCalled();
+  });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -40,10 +40,10 @@ export const MappIntelligencePlugin = NativeModules.MappinteligencePlugin
  * Builds plugin with a provided configuration. After this method finishes, plugin is ready for use.
  * @returns result if method executed succesfully or not
  */
-export function build(): Promise<number> {
+export const build = (): Promise<number> => {
   console.log('build');
   return MappIntelligencePlugin.build();
-}
+};
 
 /**
  * Initialize plugin with a provided trackIds and domain
@@ -51,42 +51,42 @@ export function build(): Promise<number> {
  * @param domain tracking domain url
  * @returns result if method executed succesfully or not
  */
-export function initWithConfiguration(
+export const initWithConfiguration = (
   trackIDs: number[],
   domain: string
-): Promise<number> {
+): Promise<number> => {
   console.log('initWithConfiguration');
   return MappIntelligencePlugin.initWithConfiguration(trackIDs, domain);
-}
+};
 
 /**
  * Set log level to define what will be logged to the console
  * @param level log level
  * @returns result if method executed succesfully or not
  */
-export function setLogLevel(level: number): Promise<number> {
+export const setLogLevel = (level: number): Promise<number> => {
   console.log('setLogLevel');
   return MappIntelligencePlugin.setLogLevel(level);
-}
+};
 
 /**
  * Set exception log level
  * @param level one of the predefined exception types
  * @returns result if method executed succesfully or not
  */
-export function setExceptionLogLevel(level: ExceptionType): Promise<number> {
+export const setExceptionLogLevel = (level: ExceptionType): Promise<number> => {
   return MappIntelligencePlugin.setExceptionLogLevel(level);
-}
+};
 
 /**
  * Sets interval in minutes, for periodic job to execute and send tracked requests to a server
  * @param interval number in minutes. The minimum is 15, limited by Android specification for a worker.
  * @returns result if method executed succesfully or not
  */
-export function setRequestInterval(interval: number): Promise<number> {
+export const setRequestInterval = (interval: number): Promise<number> => {
   console.log('setRequestInterval');
   return MappIntelligencePlugin.setRequestInterval(interval);
-}
+};
 
 /**
  * If sets to true, request will be send in a batch (multiple records in single network call);
@@ -94,50 +94,54 @@ export function setRequestInterval(interval: number): Promise<number> {
  * @param enabled speciffy if batch is enabled or disabled
  * @returns result if method executed succesfully or not
  */
-export function setBatchSupportEnabled(enabled: boolean): Promise<number> {
+export const setBatchSupportEnabled = (enabled: boolean): Promise<number> => {
   console.log('setBatchSupportEnabled');
   return MappIntelligencePlugin.setBatchSupportEnabled(enabled);
-}
+};
 
 /**
  * iOS Only - Enable sending data while application is in a background state
  * @param enabled
  * @returns result if method executed succesfully or not
  */
-export function setEnableBackgroundSendout(enabled: boolean): Promise<number> {
+export const setEnableBackgroundSendout = (
+  enabled: boolean
+): Promise<number> => {
   console.log('setEnableBackgroundSendout');
   return MappIntelligencePlugin.setEnableBackgroundSendout(enabled);
-}
+};
 
 /**
  * Set number of track records to send in a sigle batch request
  * @param size number of track records
  * @returns result if method executed succesfully or not
  */
-export function setBatchSupportSize(size: number): Promise<number> {
+export const setBatchSupportSize = (size: number): Promise<number> => {
   console.log('setBatchSupportSize');
   return MappIntelligencePlugin.setBatchSupportSize(size);
-}
+};
 
 /**
  * Requests are buffered in a queue before sending. This option set size of the queue.
  * @param numberOfRequsts size of a queue for buffering requests
  * @returns result if method executed succesfully or not
  */
-export function setRequestPerQueue(numberOfRequsts: number): Promise<number> {
+export const setRequestPerQueue = (
+  numberOfRequsts: number
+): Promise<number> => {
   console.log('setRequestPerQueue');
   return MappIntelligencePlugin.setRequestPerQueue(numberOfRequsts);
-}
+};
 
 /**
  * Control if migration should be applied from a previos SDK version.
  * @param migrate true to apply migration on the initialization process; otherwise false
  * @returns result if method executed succesfully or not
  */
-export function setShouldMigrate(migrate: boolean): Promise<number> {
+export const setShouldMigrate = (migrate: boolean): Promise<number> => {
   console.log('setShouldMigrate');
   return MappIntelligencePlugin.setShouldMigrate(migrate);
-}
+};
 
 /**
  * Based on the result of the user's conset to allow personalized tracking or not,
@@ -145,42 +149,42 @@ export function setShouldMigrate(migrate: boolean): Promise<number> {
  * @param anonymous true to enable anonymous tracking; false to disable it.
  * @returns result if method executed succesfully or not
  */
-export function setAnonymousTracking(anonymous: boolean): Promise<number> {
+export const setAnonymousTracking = (anonymous: boolean): Promise<number> => {
   console.log('setAnonymousTracking');
   return MappIntelligencePlugin.setAnonymousTracking(anonymous);
-}
+};
 
 /**
  * Send application version as parameter in every request
  * @param flag - true to set sending application version in every request; otherwise false.
  * @returns result if method executed succesfully or not
  */
-export function setSendAppVersionInEveryRequest(
+export const setSendAppVersionInEveryRequest = (
   flag: boolean
-): Promise<number> {
+): Promise<number> => {
   console.log('setSendAppVersionInEveryRequest');
   return MappIntelligencePlugin.setSendAppVersionInEveryRequest(flag);
-}
+};
 
 /**
  * To enable user matching between Engage and Intelligence system
  * @param enabled true to enable user matching; false to disable it.
  * @returns result if method executed succesfully or not
  */
-export function setEnableUserMatching(enabled: boolean): Promise<number> {
+export const setEnableUserMatching = (enabled: boolean): Promise<number> => {
   console.log('setEnableUserMatching');
   return MappIntelligencePlugin.setEnableUserMatching(enabled);
-}
+};
 
 /**
  * Track single page by page name
  * @param pageTitle page name for tracking
  * @returns result if method executed succesfully or not
  */
-export function trackPage(pageTitle: string): Promise<number> {
+export const trackPage = (pageTitle: string): Promise<number> => {
   console.log('trackPage');
   return MappIntelligencePlugin.trackPage(pageTitle);
-}
+};
 
 /**
  * Detailed page tracking with additional parameters that can be set to track
@@ -192,14 +196,14 @@ export function trackPage(pageTitle: string): Promise<number> {
  * @param campaignParameters - predefined campaign parameters
  * @returns result if method executed succesfully or not
  */
-export function trackCustomPage(
+export const trackCustomPage = (
   pageTitle: string,
   pageParameters?: PageParameters | null,
   sessionParamters?: SessionParameters | null,
   userCategories?: UserCategories | null,
   ecommerceParameters?: EcommerceParameters | null,
   campaignParameters?: CampaignParameters | null
-): Promise<number> {
+): Promise<number> => {
   console.log('trackCustomPage');
   return MappIntelligencePlugin.trackCustomPage(
     pageTitle,
@@ -209,7 +213,7 @@ export function trackCustomPage(
     convertEcommerceParameters(ecommerceParameters),
     convertCampaignParameters(campaignParameters)
   );
-}
+};
 
 /**
  * Custom page tracking with option to track some custom parameters
@@ -217,15 +221,15 @@ export function trackCustomPage(
  * @param pageParameters - custom parameters that can be tracked
  * @returns result if method executed succesfully or not
  */
-export function trackPageWithCustomData(
+export const trackPageWithCustomData = (
   pageTitle: string,
   pageParameters: Map<string, string> | null
-): Promise<number> {
+): Promise<number> => {
   console.log('trackPageWithCustomData');
   const params = pageParameters?.entries();
   const data = params != null ? Object.fromEntries(params) : {};
   return MappIntelligencePlugin.trackPageWithCustomData(data, pageTitle);
-}
+};
 
 /**
  * Track user action
@@ -237,14 +241,14 @@ export function trackPageWithCustomData(
  * @param campaignParameters - predefined campaign parameters
  * @returns result if method executed succesfully or not
  */
-export function trackAction(
+export const trackAction = (
   name: string,
   eventParameters?: EventParameters | null,
   sessionParamters?: SessionParameters | null,
   userCategories?: UserCategories | null,
   ecommerceParameters?: EcommerceParameters | null,
   campaignParameters?: CampaignParameters | null
-): Promise<number> {
+): Promise<number> => {
   console.log('trackAction');
   return MappIntelligencePlugin.trackAction(
     name,
@@ -254,7 +258,7 @@ export function trackAction(
     convertEcommerceParameters(ecommerceParameters),
     convertCampaignParameters(campaignParameters)
   );
-}
+};
 
 /**
  * Track URL's with included deeplinks, media parameters
@@ -262,24 +266,24 @@ export function trackAction(
  * @param mediaCode media code to track
  * @returns result if method executed succesfully or not
  */
-export function trackUrl(
+export const trackUrl = (
   url: string,
   mediaCode?: string | null
-): Promise<number> {
+): Promise<number> => {
   return MappIntelligencePlugin.trackUrl(url, mediaCode);
-}
+};
 
 /**
  * Track video or audio events - starting, playing, pausing/stoping, ending of playing
  * @param mediaEvent predefined events to track
  * @returns result if method executed succesfully or not
  */
-export function trackMedia(mediaEvent: MediaEvent): Promise<number> {
+export const trackMedia = (mediaEvent: MediaEvent): Promise<number> => {
   console.log('Execute MediaEvent');
   return MappIntelligencePlugin.trackMedia(
     convertMediaEvent(mediaEvent as MediaEvent)
   );
-}
+};
 
 /**
  * Record data about handled exceptions
@@ -287,16 +291,16 @@ export function trackMedia(mediaEvent: MediaEvent): Promise<number> {
  * @param stackTrace stack trace of the caught exception
  * @returns result if method executed succesfully or not
  */
-export function trackException(
+export const trackException = (
   e: Error,
   stackTrace?: string | null
-): Promise<number> {
+): Promise<number> => {
   return MappIntelligencePlugin.trackException(
     e.name,
     e.message?.slice(0, 1000),
-    e.stack !== null ? e.stack?.slice(0, 1000) : stackTrace?.slice(0, 1000)
+    stackTrace?.slice(0, 1000)
   );
-}
+};
 
 /**
  * Record data about handled exception
@@ -305,43 +309,43 @@ export function trackException(
  * @param stackTrace stack trace of the caught exception
  * @returns result if method executed succesfully or not
  */
-export function trackExceptionWithName(
+export const trackExceptionWithName = (
   name: string,
   message: string,
   stackTrace?: string | null
-): Promise<number> {
+): Promise<number> => {
   return MappIntelligencePlugin.trackException(
     name,
     message.slice(0, 1000),
     stackTrace?.slice(0, 1000)
   );
-}
+};
 
 /**
  * Set unique everId as identifier for a single device/user
  * @param everId unique identifier in the tracking system
  * @returns result if method executed succesfully or not
  */
-export function setEverId(everId?: String | null): Promise<number> {
+export const setEverId = (everId?: String | null): Promise<number> => {
   console.log('setEverId');
   return MappIntelligencePlugin.setEverId(everId);
-}
+};
 
 /**
  * Returns current everId of a device
  * @returns everId
  */
-export function getEverId(): Promise<string> {
+export const getEverId = (): Promise<string> => {
   return MappIntelligencePlugin.getEverId();
-}
+};
 
 /**
  * Check if plugin is initialized and ready to use
  * @returns true if plugin is ready to use; false otherwise.
  */
-export function isInitialized(): Promise<boolean> {
+export const isInitialized = (): Promise<boolean> => {
   return MappIntelligencePlugin.isInitialized();
-}
+};
 
 /**
  * Temporary sessionId is used when anonymous tracking is enabled to provide
@@ -349,12 +353,12 @@ export function isInitialized(): Promise<boolean> {
  * @param sessionId unique session identifier
  * @returns result if method executed succesfully or not
  */
-export function setTemporarySessionId(
+export const setTemporarySessionId = (
   sessionId?: String | null
-): Promise<number> {
+): Promise<number> => {
   console.log('setTemporarySessionId');
   return MappIntelligencePlugin.setTemporarySessionId(sessionId);
-}
+};
 
 /**
  * In some cases, it is necessary to exclude users completely from tracking.
@@ -363,26 +367,26 @@ export function setTemporarySessionId(
  * @param sendData true to send recorded data before opt-out
  * @returns result if method executed succesfully or not
  */
-export function optOut(sendData: boolean): Promise<number> {
+export const optOut = (sendData: boolean): Promise<number> => {
   return MappIntelligencePlugin.optOut(sendData);
-}
+};
 
 /**
  * Disables opt-out, and resets tracking to enabled.
  * @param sendData true to send recorded data before opt-in
  * @returns result if method executed succesfully or not
  */
-export function optIn(sendData: boolean): Promise<number> {
+export const optIn = (sendData: boolean): Promise<number> => {
   return MappIntelligencePlugin.optIn(sendData);
-}
+};
 
 /**
  * Reset all webtrekk configuration. After this, new init with settings must be called before using plugin.
  * @returns result if method executed succesfully or not
  */
-export function reset(): Promise<number> {
+export const reset = (): Promise<number> => {
   return MappIntelligencePlugin.reset();
-}
+};
 
 /**
  * When called, data will be immediately sent.
@@ -390,10 +394,14 @@ export function reset(): Promise<number> {
  * Please note that the application must be started and visible to use this method.
  * @returns result if method executed succesfully or not
  */
-export function sendRequestsAndClean(): Promise<number> {
+export const sendRequestsAndClean = (): Promise<number> => {
   return MappIntelligencePlugin.sendRequestsAndClean();
-}
+};
 
-export function printCurrentConfig(): Promise<string> {
+/**
+ * Get active configuration of the native SDK and returns it as a string
+ * @returns string representation of the active configuration
+ */
+export const printCurrentConfig = (): Promise<string> => {
   return MappIntelligencePlugin.getCurrentConfig();
-}
+};
